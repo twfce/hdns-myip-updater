3fa023d444407a5a17bc80a277a9a8186af7c07c
Johannes <johannes@seitle.io>
2023-03-06 19:18:26 +0100

    Now using multiple IPs in case one is not reachable

--- src/myip.py
+++ src/myip.py
@@ -1,18 +1,20 @@
 import json
 import requests
+import random
 
 class myip():
     def __init__(self):
-        self.baseUrl = "https://api.myip.com"
+        self.baseUrls = ["https://api.myip.com","https://api.ipify.org?format=json","https://api.my-ip.io/ip.json"]
 
     def requestIP(self):
         __name__ = "requestIP"
-        try:
+
+        random.shuffle(self.baseUrls)
+        for url in self.baseUrls:
+            print('[{name}] Sending request to {url}'.format(name=__name__, url=url))
             response = requests.get(
-                url="{baseUrl}".format(baseUrl=self.baseUrl)
+                url=url
             )
-            print('[{name}] Response HTTP Status Code: {status_code}'.format(
-                name=__name__, status_code=response.status_code))
-            return json.loads(response.content)
-        except requests.exceptions.RequestException:
-            print('HTTP Request failed')
+            print('[{name}] Response HTTP Status Code: {status_code}'.format(name=__name__, status_code=response.status_code))
+            if response.status_code == 200:
+                return json.loads(response.content)
